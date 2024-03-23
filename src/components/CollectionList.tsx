944e9aa1249ec1b1f19bf483403bcfa374ec1b6c
Кирилл Попов <diplomatgmg@gmail.com>
2024-03-23 20:28:31 +0300

    Оптимизация рендеринга списка коллекций

--- src/components/CollectionList.tsx
+++ src/components/CollectionList.tsx
@@ -5,34 +5,32 @@ import { useAppSelector } from '../redux/hooks'
 import CollectionItemSkeleton from './CollectionItemSkeleton'
 
 const CollectionList = (): ReactElement => {
-  const selectedCategoryId = useAppSelector(state => state.collection.selectedCategoryId)
-
-  const searchValue = useAppSelector(state => state.collection.searchValue)
-
-  const {
-    data: collection,
-    isLoading,
-    isFetching,
-    isError
-  } = useGetPhotosQuery({ category: selectedCategoryId === 1 ? '' : selectedCategoryId })
+  const { selectedCategoryId, searchValue } = useAppSelector(state => state.collection)
+  const { data: collection = [], isLoading, isFetching, isError } = useGetPhotosQuery({ category: selectedCategoryId === 1 ? '' : selectedCategoryId })
 
   if (isError) {
     return <h1 style={{ marginTop: '32px' }}>Нет коллекций.</h1>
   }
 
+  const renderSkeleton = (): ReactElement => (
+    <>
+      {Array.from({ length: 8 }).map((_, index) => <CollectionItemSkeleton key={index}/>)}
+    </>
+  )
+
+  const renderCollection = (): ReactElement => {
+    const filterCollection = collection.filter(({ name }) => searchValue === '' || name.toLowerCase().includes(searchValue.toLowerCase()))
+
+    return (
+      <>
+        {filterCollection.map(({ id, name, images }) => <CollectionItem key={id} name={name} images={images}/>)}
+      </>
+    )
+  }
+
   return (
     <div className="content">
-      {
-        isLoading || isFetching
-          ? (
-              Array.from({ length: 8 }).map((_, index) => <CollectionItemSkeleton key={index}/>)
-            )
-          : (
-              collection
-                ?.filter(({ name }) => searchValue === '' || name.toLowerCase().includes(searchValue.toLowerCase()))
-                ?.map(({ id, name, images }) => <CollectionItem key={id} name={name} images={images}/>)
-            )
-      }
+      {isLoading || isFetching ? renderSkeleton() : renderCollection()}
     </div>
   )
 }
