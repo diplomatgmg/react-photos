4cdce5e47934ab3ab23baef72bf2a233f62f8724
Кирилл Попов <diplomatgmg@gmail.com>
2024-03-23 19:42:14 +0300

    Рефакторинг

--- src/components/CollectionItemSkeleton.tsx
+++ src/components/CollectionItemSkeleton.tsx
@@ -3,11 +3,11 @@ import React, { type ReactElement } from 'react'
 const CollectionItemSkeleton = (): ReactElement => {
   return (
     <div className="collection">
-      <img className="collection__big skeleton" />
+      <img className="collection__big skeleton" alt=""/>
       <div className="collection__bottom">
-        <img className="collection__mini skeleton" />
-        <img className="collection__mini skeleton"/>
-        <img className="collection__mini skeleton"/>
+        <img className="collection__mini skeleton" alt=""/>
+        <img className="collection__mini skeleton" alt=""/>
+        <img className="collection__mini skeleton" alt=""/>
       </div>
       <h4 className="skeleton__text"></h4>
     </div>
